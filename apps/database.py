f120404b7c79b34fb1775fb2a24cc5f15c226c90
user22 <goodsirhello807@gmail.com>
2025-06-11 23:18:43 +0530

    Added the internal transaction db to keep a record of internal transactions

--- apps/database.py
+++ apps/database.py
@@ -4,3 +4,4 @@ from tinydb import TinyDB
 userDb = TinyDB("database/users.json")
 transactionDb = TinyDB("database/transactions.json")
 financeDb = TinyDB("database/finance.json")
+internalTransactionDb = TinyDB("database/internalTransactions.json")
