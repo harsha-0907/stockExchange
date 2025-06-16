5bdf5ad370bf6d7650b6ce760f9fcc1026d89822
arakkutrip1612 <arakkutrip1612@gmail.com>
2025-06-16 10:28:51 +0000

    Updated the file operations from w to a to support persistent ops

--- apps/database.py
+++ apps/database.py
@@ -1,15 +1,16 @@
 
 from tinydb import TinyDB
 
-with open("database/users.json", 'w') as file:
+with open("database/users.json", 'a') as file:
     pass
-with open("database/transactions.json", 'w') as file:
+with open("database/transactions.json", 'a') as file:
     pass
-with open("database/finance.json", 'w') as file:
+with open("database/finance.json", 'a') as file:
     pass
-with open("database/internalTransactions.json", 'w') as file:
+with open("database/internalTransactions.json", 'a') as file:
     pass
 userDb = TinyDB("database/users.json")
 transactionDb = TinyDB("database/transactions.json")
 financeDb = TinyDB("database/finance.json")
 internalTransactionDb = TinyDB("database/internalTransactions.json")
+
